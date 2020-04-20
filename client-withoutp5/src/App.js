edc939fe4ed5818f30498f95cfd557b8cdb7a4d5
umbone <henarivanaagit@gmail.com>
2020-04-20 13:18:06 +0700

    Changes: - Setting up routes for Home and Game - Fixing src for fruits assets in Home.js and page.css

--- client-withoutp5/src/App.js
+++ client-withoutp5/src/App.js
@@ -1,10 +1,38 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from 'react-router-dom';
+import Home from './pages/Home';
 import Camera from './pages/Camera';
+
+const routes = [
+  {
+    path: '/',
+    exact: true,
+    children: <Home />
+  },
+  {
+    path: '/game',
+    exact: true,
+    children: <Camera />
+  }
+];
 
 function App() {
   return (
     <>
-    <Camera />
+      <Router>
+        <Switch>
+          {routes.map((route, idx) => {
+            return (
+              <Route key={idx} {...route} />
+            )
+          })}
+        </Switch>
+      </Router>
+      {/* <Camera /> */}
     </>    
   );
 }
--- client-withoutp5/src/pages/Home.js
+++ client-withoutp5/src/pages/Home.js
@@ -1,32 +1,36 @@
-import React from 'react';
-import '../page.css'
-import apple from '.../public/assets/Apple.png';
-import banana from './public/assets/Banana.png';
-import grapes from './public/assets/Grape.png';
-import orange from './public/assets/Orange.png';
-import pear from './public/assets/Pear.png';
-import strawberry from './public/assets/Strawberry.png'
-import watermelon from './public/assets/WaterMelon.png'
+import React from "react";
+import "../page.css";
+import { useHistory } from "react-router-dom";
+const strawberry = "/assets/fruits/Strawberry.png";
+const grapes = "/assets/fruits/Grape.png";
+const banana = "/assets/fruits/Banana.png";
+const orange = "./assets/fruits/Orange.png";
+const watermelon = "./assets/fruits/WaterMelon.png";
+const apple = "./assets/fruits/Apple.png";
+const pear = "./assets/fruits/Pear.png";
 
-function Home(){
-    return (
-        <div className="Body">
-          <img src={strawberry} className="strawberry rotate" alt="fruit" />
-          <img src={grapes} className="grapes rotatereverse" alt="fruit" />
-          <img src={banana} className="banana rotate" alt="fruit" />
-          <img src={orange} className="orange rotatereverse" alt="fruit" />
-          <div className="center">
-            <div className="bounce">
-            <h3>Welcome to Slash-It !</h3>
-            <div className="btn btn-danger"><h4>Play Now</h4></div>
-            </div>
+function Home() {
+  const history = useHistory();
+  return (
+    <div className="Body">
+      <img src={strawberry} className="strawberry rotate" alt="fruit" />
+      <img src={grapes} className="grapes rotatereverse" alt="fruit" />
+      <img src={banana} className="banana rotate" alt="fruit" />
+      <img src={orange} className="orange rotatereverse" alt="fruit" />
+      <div className="center">
+        <div className="bounce">
+          <h3>Welcome to Slash-It !</h3>
+          <div className="btn btn-danger">
+            <h4 style={{ cursor: 'pointer' }} onClick={() => history.push('/game')}>Play Now</h4>
           </div>
-          <img src={watermelon} className="watermelon rotate" alt="fruit" />
-          <img src={orange} className="strawberry2 rotatereverse" alt="fruit" />
-          <img src={apple} className="apple rotatereverse" alt="fruit" />
-          <img src={pear} className="pear rotate" alt="fruit" />
         </div>
-    );
+      </div>
+      <img src={watermelon} className="watermelon rotate" alt="fruit" />
+      <img src={orange} className="strawberry2 rotatereverse" alt="fruit" />
+      <img src={apple} className="apple rotatereverse" alt="fruit" />
+      <img src={pear} className="pear rotate" alt="fruit" />
+    </div>
+  );
 }
 
-export default Home
+export default Home;
