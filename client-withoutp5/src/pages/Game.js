10b225d88abe34503d916a09867d0e30ad637bd4
agunggst <rqz.agastya@gmail.com>
2020-04-20 11:23:17 +0700

    Changes: 1. Menambahkan Bomb di playzone kanan dan kiri 2. Calibrasi setting difficulty game 3. Transparent playzone

--- client-withoutp5/src/pages/Game.js
+++ client-withoutp5/src/pages/Game.js
@@ -1,16 +1,42 @@
 import React, { useState, useEffect } from "react";
 import { useSelector } from "react-redux";
-import collideCircle from '../helpers/collideCircle';
-import { createHandKeypoint } from '../helpers/extend';
+import collideCircle from "../helpers/collideCircle";
+import { createHandKeypoint } from "../helpers/extend";
 import Sketch from "react-p5";
 import FruitLeft from "../objects/fruitLeft";
 import FruitRight from "../objects/fruitRight";
+import BombLeft from "../objects/bomLeft";
+import BombRight from "../objects/bombRight";
 
 const music = new Audio('/assets/audio/GameBg.mp3');
 
 const Game = ({ width, height }) => {
   const [fruits, setFruits] = useState([]);
-  const [boundary, setBoundary] = useState(300);
+  const [bombs, setBombs] = useState([]);
+  const [boundary, setBoundary] = useState(400);
+  const [gameMode, setGameMode] = useState(2);
+  const [gameConfig, setGameConfig] = useState(
+    [
+      {
+        fruitTriggerConstant: 0.98,
+        bombTriggerConstant: 0.995,
+        vyRandomFactor: 15,
+        gravity: 0.15
+      },
+      {
+        fruitTriggerConstant: 0.97,
+        bombTriggerConstant: 0.99,
+        vyRandomFactor: 20,
+        gravity: 0.3
+      },
+      {
+        fruitTriggerConstant: 0.96,
+        bombTriggerConstant: 0.99,
+        vyRandomFactor: 24,
+        gravity: 0.4
+      }
+    ]
+  );
 
   const calibrated = useSelector((state) => state.keypoint.calibrated);
   const keypoints = useSelector((state) => state.keypoint.keypoints);
@@ -33,7 +59,7 @@ const Game = ({ width, height }) => {
           collideCircle(
             letfHandKeypoints.x,
             letfHandKeypoints.y,
-            150,
+            100,
             fruit.x,
             fruit.y,
             fruit.diameter
@@ -50,7 +76,7 @@ const Game = ({ width, height }) => {
           collideCircle(
             rightHandKeypoints.x,
             rightHandKeypoints.y,
-            150,
+            100,
             fruit.x,
             fruit.y,
             fruit.diameter
@@ -74,22 +100,50 @@ const Game = ({ width, height }) => {
 
   const draw = (p5) => {
     p5.clear()
-    if(Math.random() >= 0.98){
-      setFruits([...fruits, new FruitLeft(p5, boundary)]);
-    }
-    if(Math.random() >= 0.98){
-      setFruits([...fruits, new FruitRight(p5, boundary)]);
-    }
+    if(Math.random() >= gameConfig[gameMode].fruitTriggerConstant){
+      setFruits([...fruits, new FruitLeft(
+        p5, 
+        boundary, 
+        gameConfig[gameMode].gravity, 
+        gameConfig[gameMode].vyRandomFactor,
+      )]);
+    };
+    if(Math.random() >= gameConfig[gameMode].fruitTriggerConstant){
+      setFruits([...fruits, new FruitRight(
+        p5, 
+        boundary, 
+        gameConfig[gameMode].gravity, 
+        gameConfig[gameMode].vyRandomFactor,
+      )]);
+    };
+    if(Math.random() >= gameConfig[gameMode].bombTriggerConstant){
+      setBombs([...bombs, new BombLeft(
+        p5,
+        boundary
+      )]);
+    };
+    if(Math.random() >= gameConfig[gameMode].bombTriggerConstant){
+      setBombs([...bombs, new BombRight(
+        p5,
+        boundary
+      )]);
+    };
     
     for(let fruit of fruits){
       fruit.show()
       fruit.move()
-    }
+    };
 
-    p5.fill(240, 0, 0)
+    for(let bomb of bombs){
+      bomb.show()
+      bomb.move()
+    };
+
+    p5.noStroke();
+    p5.fill(75, 75, 75, 100);
     
-    p5.rect(boundary, 0, 2, p5.height-5)
-    p5.rect(p5.width - boundary, 0, 2, p5.height-5)
+    p5.rect(0, 0, boundary, p5.height-5);
+    p5.rect(p5.width - boundary, 0, boundary, p5.height-5);
   }
 
   return(
