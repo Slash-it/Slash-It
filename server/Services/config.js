053f4a4fa72f4bd19a8ed429d49e6ef37cac692d
afif julio <afifjulio28@gmail.com>
2020-04-20 08:17:46 +0700

    creating backend test

--- server/Services/config.js
+++ server/Services/config.js
@@ -1,5 +1,5 @@
 const { MongoClient } = require('mongodb')
 const url = 'mongodb://localhost:27017'
-const client = new MongoClient(url,{useUnifiedTopology:true})
+const client = new MongoClient(url,{useNewUrlParser: true,useUnifiedTopology:true})
 
 module.exports = client
